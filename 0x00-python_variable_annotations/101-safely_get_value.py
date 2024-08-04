4df5a8826c8452016c20d2021b49649f3af4faab
SafaFarah <kangjisook13@gmail.com>
2024-08-04 21:28:49 +0300

    create 102-type_checking.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,7 +7,7 @@ from typing import TypeVar, Mapping, Any, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any, Any], key: Any,
+def safely_get_value(dct: Mapping, key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
     """
     Safely retrieves a value from a dict using a key, with an optional default.
