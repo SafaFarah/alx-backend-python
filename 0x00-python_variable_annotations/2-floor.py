7e88074ca2e2e5fa0c79c8a70e2ee82236c3e73c
SafaFarah <kangjisook13@gmail.com>
2024-08-04 12:58:47 +0300

    create files for tasks 0-4

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to calculate the floor of a float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the float n.
+
+    Args:
+        n (float): The number to be floored.
+
+    Returns:
+        int: The floor of the number.
+    """
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+ module provides a function to convert a float to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of the float n.
+
+    Args:
+        n (float): The float number to convert to string.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
